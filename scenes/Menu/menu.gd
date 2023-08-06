c6470a40f8ea2b9d34b8d7ab66aa3f67dd25b318
Vinícius Hoyer <contact@vhoyer.dev>
2023-08-06 12:26:47 -0300

    :truck: Move main scenes to theirs own folders

--- scenes/Menu/menu.gd
+++ scenes/Menu/menu.gd
@@ -4,5 +4,5 @@ extends Node2D
 
 func _on_start_button_down():
 	$CanvasLayer.visible = false
-	add_child(preload("res://scenes/First.tscn").instantiate())
+	add_child(preload("res://scenes/First/First.tscn").instantiate())
 	pass
