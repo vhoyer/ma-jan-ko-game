741ab4c7168f2de866b75ba375013e5454ad1026
Vinícius Hoyer <contact@vhoyer.dev>
2023-08-06 19:24:05 -0300

    :sparkles: Fix pixel perfect movement of the player
    with a little help from reddit

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 25
 @export var contain_in_viewport: bool = false
+@export var pixel_perfect_movement: bool = true
 
 func _ready():
 	$AnimationPlayer.play("RESET_down")
@@ -9,7 +10,19 @@ func _ready():
 func _physics_process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down").normalized()
 	velocity = direction * SPEED
+	var old = position
 	move_and_slide()
+	
+	if pixel_perfect_movement and velocity and !is_on_wall():
+		# https://www.reddit.com/r/godot/comments/cvn6qn/ive_figured_out_a_way_to_smooth_out_jittery/?utm_source=share&utm_medium=android_app&utm_name=androidcss&utm_term=3&utm_content=1
+		if abs(old.x - position.x) > abs(old.y - position.y):
+			var x = round(position.x)
+			var y = round(position.y + (x - position.x) * velocity.y / velocity.x)
+			position.y = y
+		else:
+			var y = round(position.y)
+			var x = round(position.x + (y - position.y) * velocity.x / velocity.y)
+			position.x = x
 	
 	if velocity.length() > 0:
 		var animation: String
